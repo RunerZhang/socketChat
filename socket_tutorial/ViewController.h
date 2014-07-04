0f48ca3e2fd9b6aa11f98df06b1f877903923c58
xiaoliangwang <xiaoliangwang@xiaoliangwangdeiMac.local>
2014-07-04 10:20:32 +0800

    Initial Commit

---
+++ socket_tutorial/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  socket_tutorial
+//
+//  Created by xiaoliangwang on 14-7-4.
+//  Copyright (c) 2014年 芳仔小脚印. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ socket_tutorial/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  socket_tutorial
+//
+//  Created by xiaoliangwang on 14-7-4.
+//  Copyright (c) 2014年 芳仔小脚印. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
